0c0f5534f5293ec774ac5bf6d35a6f7662bf4f3f
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-04-20 23:39:09 +0800

    bugfix - module import

--- components/playground/tool/index.js
+++ components/playground/tool/index.js
@@ -1,16 +1,14 @@
-
-
-
-
-export function moveCursorWithinInput(input, position) {
+function moveCursorWithinInput(input, position) {
   if (input.setSelectionRange) {
     input.focus();
     input.setSelectionRange(position, position);
   } else if (input.createTextRange) {
     var range = input.createTextRange();
     range.collapse(true);
-    range.moveEnd('character', position);
-    range.moveStart('character', position);
+    range.moveEnd("character", position);
+    range.moveStart("character", position);
     range.select();
   }
 }
+
+export default moveCursorWithinInput;
