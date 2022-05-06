cceca6314be88bad4ba3927d664b31aeee258663
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-05-06 20:34:00 +0800

    bugfix config module

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: false,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   eslint: {
-    dirs: ['pages', 'components', 'lib', 'layouts', 'scripts'],
+    dirs: ['pages', 'components', 'lib', 'layouts', 'scripts', 'config'],
   },
   
   webpack: (config, { dev, isServer }) => {
