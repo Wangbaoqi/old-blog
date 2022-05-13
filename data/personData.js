cc9e69282ec47c893f75f764335230fc7edd632c
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-05-13 15:39:26 +0800

    feat update prisma database

--- data/personData.js
+++ data/personData.js
@@ -22,3 +22,6 @@ export default personData;
 // username: 1m632bovq0to
 // host: vp03qkgpra1y.ap-northeast-2.psdb.cloud
 // password: pscale_pw_2BhZtp-qBsS06WXTBsEbnY1u0aMVz27HalTcjPidcSU
+
+
+// pscale_pw_kVZOi2RZuEDBQEWC3vjbMRL2dPJQewTypkgv70MHRqY
