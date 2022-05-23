425a5424fd851b278667340f8597a59200eb6d97
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-05-23 15:40:41 +0800

    fix seo sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -3,23 +3,6 @@ import { getServerSideSitemap } from 'next-sitemap';
 
 export const getServerSideProps = async (ctx) => {
 
-  const baseUrl = 'http://localhost:3000'
-
-  // const staticPages = fs
-  //   .readdirSync("pages")
-  //   .filter((staticPage) => {
-  //     return ![
-  //       "_app.js",
-  //       "_document.js",
-  //       "_error.js",
-  //       "sitemap.xml.js",
-  //     ].includes(staticPage);
-  //   })
-  //   .map((staticPagePath) => {
-  //     return `${baseUrl}/${staticPagePath}`;
-  //   });
-  
-  // console.log(staticPages, 'DD');
   
   const fields = [
     {
