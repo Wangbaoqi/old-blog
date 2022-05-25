63374b867e3eeac82c2be24cc481164635b87483
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-05-25 14:14:59 +0800

    feat add sitemap

---
+++ script/build-generate-sitemap.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+const prettier = require('prettier');
+
+const siteUrl = "https://www.wangbaoqi.tech";
+
+; (async () => {
+  const { globby } = await import("globby");
+
+  const pages = await globby([
+    "pages/*.js",
+    "posts/**/*.mdx",
+    "posts/**/*.md",
+    "!pages/_*.js",
+    "!pages/_*.tsx",
+  ]);
+
+  const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          ${pages.map((page) => {
+            const path = page
+              .replace("pages/", "/")
+              .replace("posts/", "/posts/")
+              .replace(".js", "")
+              .replace(".tsx", "")
+              .replace(".mdx", "");
+
+            return `
+            <url>
+                <loc>${siteUrl}${path}</loc>
+            </url>
+            `;
+          })}
+        </urlset>
+  `;
+
+
+  const formatted = prettier.format(sitemap, {
+    parser: 'html',
+  })
+  console.log(formatted, "sitemap");
+  fs.writeFileSync('public/sitemap.xml', formatted)
+
+})();
