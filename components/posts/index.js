272be384ed311633f158e97c222cf0ce7d02b14e
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-06-21 09:33:13 +0800

    fix: update import

--- components/posts/index.js
+++ components/posts/index.js
@@ -6,12 +6,9 @@ export { default as RecentCard } from './recentCard';
 export { default as RecentPost } from './recentPost';
 export { default as RecomendWrapper } from './recomendWrapper';
 export { default as RecomendPost } from './recomendPost';
-
 export { default as NextPost } from './nextPost';
 export { default as CategoryWrapper } from './categoryWrapper';
-
 export { default as AlgorithmWrapper } from './algorithmWrapper';
-
 export { default as SnippetCard } from './snippetCard';
 export { default as SnippetWrapper } from './snippetWrapper';
 
--- components/ui/index.js
+++ components/ui/index.js
@@ -2,15 +2,12 @@ export { default as Avatar } from './avatar'
 export { default as Logo } from './logo'
 export { default as Title } from './title'
 export { default as Category } from './category'
-export { default as CategoryInner } from './category_inner'
+export { default as CategoryInner } from './categoryInner'
 export { default as GithubIcon } from './githubIcon'
 export { default as Img } from './image'
 
-
 export { default as SearchBtn } from './searchIcon'
-
 export { default as Input } from './input'
 export { default as Select } from './select'
-
 export { default as PerPage } from './perPage'
 
