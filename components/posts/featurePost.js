6041561e94c8a54e33a16db3eba991826f9d3b72
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-06-30 10:46:12 +0800

    fix: tailwind css line

--- components/posts/featurePost.js
+++ components/posts/featurePost.js
@@ -8,6 +8,17 @@ export default function FeaturePost({
   slug,
 }) {
 
+
+  let lineCls = 'line-clamp-3';
+  switch (lineNum) {
+    case 2:
+      lineCls = 'line-clamp-2'
+      break;
+    default:
+      break;
+  }
+  
+
   return (
     <article className={`cursor-pointer group overflow-hidden`}>
       <Link className='relative flex flex-col justify-between' href={`/posts/${slug}`}>
@@ -15,7 +26,7 @@ export default function FeaturePost({
           <h3 className="md:text-xl text-md leading-snug dark:text-white group-hover:text-second-color">
             {title}
           </h3>
-          <p className={`text-sm my-3 leading-8 pr-4 line-clamp-${lineNum}`}>{excerpt}</p>
+          <p className={`text-sm my-3 leading-8 pr-4 ${lineCls}`}>{excerpt}</p>
           <p className='dark:text-white font-medium flex items-center'>
             <span>Read more</span>
             <ArrowRight size={15} className='ml-2 hidden group-hover:block text-second-color'/>
